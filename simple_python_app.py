1bf84167f1f10fe28503b70170d96fe5089eedb9
pozbliz <sebro@hushmail.com>
2024-02-10 23:19:50 +0900

    changed app and compose yaml

--- simple_python_app.py
+++ simple_python_app.py
@@ -1,1 +1,12 @@
-print("Hello World!!")
+# print("Hello World!!")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
