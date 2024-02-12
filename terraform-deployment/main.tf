1f18daac1446e1c4dd6f7a9193965cea48310a28
pozbliz <sebro@hushmail.com>
2024-02-12 13:31:53 +0900

    changed tf config

--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "terraform_practice" {
+  name = "pozbliz/terraform_practice:latest"
+}
+
 resource "docker_container" "py-app-container" {
   name  = "flask_app"
-  image = "pozbliz/terraform_practice:latest"
+  image = docker_image.terraform_practice.name
 
 }
