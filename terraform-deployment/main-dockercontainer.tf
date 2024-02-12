f4944d1b4b48495b25ce016c816e72edda587180
pozbliz <sebro@hushmail.com>
2024-02-12 18:49:43 +0900

    changed main.tf for aws

---
+++ terraform-deployment/main-dockercontainer.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+## Implicit provider detection can occur if tf finds Docker socket or API on system,
+## but it is recommended to explicitly declare provider configuration
+#provider "docker" {
+#  host    = "npipe:////.//pipe//docker_engine"
+#}
+
+# Specify image
+resource "docker_image" "terraform_practice" {
+  name = "pozbliz/terraform_practice:latest"
+}
+
+# Specify container
+resource "docker_container" "py-app-container" {
+  name  = "flask_app"
+  image = docker_image.terraform_practice.name
+  ports {
+    internal = 8000
+    external = 8000
+  }
+}
--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -1,29 +1,22 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 3.0.1"
-    }
-  }
-}
-
-## Implicit provider detection can occur if tf finds Docker socket or API on system,
-## but it is recommended to explicitly declare provider configuration
-#provider "docker" {
-#  host    = "npipe:////.//pipe//docker_engine"
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 5.0"
+#    }
+#  }
 #}
 
-# Specify image
-resource "docker_image" "terraform_practice" {
-  name = "pozbliz/terraform_practice:latest"
+# Provider block
+provider "aws" {
+  region     = "ap-northeast-1"
 }
 
-# Specify container
-resource "docker_container" "py-app-container" {
-  name  = "flask_app"
-  image = docker_image.terraform_practice.name
-  ports {
-    internal = 8000
-    external = 8000
+# Resources block
+resource "aws_instance" "app_server" {
+  ami = "ami-0b5c74e235ed808b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-instance"
   }
 }
